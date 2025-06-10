a0b6b0c3b8b488452815ff9670feb845e99d66c5
Petr Velichkin <pyo7578@yandex.ru>
2025-06-10 18:21:01 +0300

    feat: Добавил точку входа приложения

---
+++ TicTacToe.py
@@ -0,0 +1,13 @@
+from src.Game import Game
+
+
+
+class TicTacToe:
+
+    @staticmethod
+    def main():
+        game = Game()
+        game.run()
+
+if __name__ == '__main__':
+    TicTacToe.main()
